7f89a4ce985004f045e0e6f19605090c2a4a4622
baobaosieudebug <65277503+baobaosieudebug@users.noreply.github.com>
2021-09-26 13:43:28 +0700

    edit structure server

--- src/models/Content.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Node = require('../models/Node');
-const ContentSchema = new mongoose.Schema({
-    content:[Node],
-});
-
-const Content= mongoose.model('content',ContentSchema);
-module.exports = Content;
